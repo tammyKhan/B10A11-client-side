181cc51bfa08fa8d7c82790763c19e436bc21b37
tammykhan <tammykhan543@gmail.com>
2025-02-15 15:42:30 +0600

    created FoodCard page

---
+++ src/components/FoodCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const FoodCard = ({food}) => {
+  return (
+    <div  className="card bg-base-100 shadow-xl">
+    <figure>
+      <img src={food.foodImage} alt={food.foodName} className="w-full h-48 object-cover" />
+    </figure>
+    <div className="card-body">
+      <h3 className="card-title text-lg font-semibold">{food.foodName}</h3>
+      <p className="text-gray-600">{food.additionalNotes.slice(0, 85)} ....</p>
+      <div className="card-actions justify-between my-2">
+        <span className="text-xs mb-2 font-semibold">Expired Date: {food.expireDate}</span>
+        <Link to={`/food-details/${food._id}`}
+          className="btn border-2 border-red-700 text-red-700 "
+        >
+          View Details
+        </Link>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default FoodCard;
--- src/pages/AvailableFoods.jsx
+++ src/pages/AvailableFoods.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { AuthContext } from "../provider/AuthProvider";
+import FoodCard from "../components/FoodCard";
 
 const AvailableFoods = () => {
   const { user } = useContext(AuthContext); 
@@ -29,14 +30,6 @@ const AvailableFoods = () => {
     setSortByExpiry(!sortByExpiry); // Sort Toggle
   };
 
-  const handleViewDetails = (foodId) => {
-    if (!user) {
-      navigate('/login'); // Redirect to login if user is not logged in
-    } else {
-      navigate(`/food-details/${foodId}`); // Navigate to food details page
-    }
-  };
-
   return (
     <div className=" container mx-auto p-6">
       <h2 className="text-2xl flex items-center justify-center font-bold mb-4">Available Foods</h2>
@@ -48,24 +41,10 @@ const AvailableFoods = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
         {foods.map((food) => (
-          <div key={food._id} className="card bg-base-100 shadow-xl">
-          <figure>
-            <img src={food.foodImage} alt={food.foodName} className="w-full h-48 object-cover" />
-          </figure>
-          <div className="card-body">
-            <h3 className="card-title text-lg font-semibold">{food.foodName}</h3>
-            <p className="text-gray-600">{food.additionalNotes.slice(0, 85)} ....</p>
-            <div className="card-actions justify-between my-2">
-              <span className="text-xs mb-2 font-semibold">Expired Date: {food.expireDate}</span>
-              <button
-                className="btn border-2 border-red-700 text-red-700 "
-                onClick={() => handleViewDetails(food._id)}
-              >
-                View Details
-              </button>
-            </div>
-          </div>
-        </div>
+           <FoodCard
+           key={food._id}
+           food={food}
+           ></FoodCard>
         ))}
       </div>
     </div>
