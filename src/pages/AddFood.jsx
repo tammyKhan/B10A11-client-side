ca33b3e4c8e6d16bfe88e810b1c27d9d557aa595
tammykhan <tammykhan543@gmail.com>
2025-02-15 08:53:42 +0600

    Created Available Foods page

--- src/pages/AddFood.jsx
+++ src/pages/AddFood.jsx
@@ -1,5 +1,4 @@
 
-
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import { useNavigate } from "react-router-dom";
--- src/pages/AvailableFoods.jsx
+++ src/pages/AvailableFoods.jsx
@@ -1,20 +1,68 @@
-import React from 'react';
-import FeaturedFoods from '../components/FeaturedFoods';
+import React, { useContext } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { AuthContext } from "../provider/AuthProvider";
 
 const AvailableFoods = () => {
-  // Sample data for featured foods
-const featuredFoods = [
-  { id: 1, name: 'Pizza', description: 'Delicious cheese pizza', price: 9.99, imageUrl: 'https://via.placeholder.com/400' },
-  { id: 2, name: 'Burger', description: 'Juicy beef burger', price: 7.49, imageUrl: 'https://via.placeholder.com/400' },
-  { id: 3, name: 'Pasta', description: 'Spaghetti with marinara sauce', price: 12.99, imageUrl: 'https://via.placeholder.com/400' },
-  { id: 4, name: 'Sushi', description: 'Fresh salmon sushi', price: 14.99, imageUrl: 'https://via.placeholder.com/400' },
-  { id: 5, name: 'Salad', description: 'Healthy green salad', price: 5.99, imageUrl: 'https://via.placeholder.com/400' },
-  { id: 6, name: 'Sandwich', description: 'Grilled chicken sandwich', price: 6.49, imageUrl: 'https://via.placeholder.com/400' },
-];
+  const { user } = useContext(AuthContext); // Check if the user is logged in
+  const navigate = useNavigate();
+  const [foods, setFoods] = useState([]);
+  const [sortByExpiry, setSortByExpiry] = useState(false);
+  
+
+  useEffect(() => {
+    const fetchFoods = async () => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/food`);
+        setFoods(response.data);
+      } catch (error) {
+        console.error("Error fetching foods:", error);
+      }
+    };
+    fetchFoods();
+  }, []);
+
+  const handleSort = () => {
+    const sortedFoods = [...foods].sort((a, b) => new Date(a.expireDate) - new Date(b.expireDate));
+    setFoods(sortedFoods);
+    setSortByExpiry(!sortByExpiry);
+  };
+
+  const handleViewDetails = (foodId) => {
+    if (!user) {
+      navigate('/login'); // Redirect to login if user is not logged in
+    } else {
+      navigate(`/food-details/${foodId}`); // Navigate to food details page
+    }
+  };
 
   return (
-    <div>
-      <FeaturedFoods featuredFoods={featuredFoods}></FeaturedFoods>
+    <div className=" container mx-auto p-6">
+      <h2 className="text-2xl flex items-center justify-center font-bold mb-4">Available Foods</h2>
+  
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {foods.map((food) => (
+          <div key={food._id} className="card bg-base-100 shadow-xl">
+          <figure>
+            <img src={food.foodImage} alt={food.foodName} className="w-full h-48 object-cover" />
+          </figure>
+          <div className="card-body">
+            <h3 className="card-title text-lg font-semibold">{food.foodName}</h3>
+            <p className="text-gray-600">{food.additionalNotes.slice(0, 85)} ....</p>
+            <div className="card-actions justify-between my-2">
+              <span className="text-xs mb-2 font-semibold">Expired Date: {food.expireDate}</span>
+              <button
+                className="btn border-2 border-red-700 text-red-700 "
+                onClick={() => handleViewDetails(food._id)}
+              >
+                View Details
+              </button>
+            </div>
+          </div>
+        </div>
+        ))}
+      </div>
     </div>
   );
 };
